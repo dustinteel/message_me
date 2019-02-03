bcfcb67fb55444b4211b47dcb6cead9c7f7e97eb
Dustin Teel <Dustin.Teel@leidos.com>
2019-02-03 01:26:38 -0500

    create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,24 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      # good scenario
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      # failure scenario
+      flash.now[:error] = "Invalid username or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
+
 end
